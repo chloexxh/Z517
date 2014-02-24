0e51a56f3344e7676763bfce92f4d4b24ba81d0f
chloexxh <chloexxh@gmail.com>
2014-02-23 22:56:30 -0500

    finish:A1

--- IndividualAssignment/src/A1_Pizzeria.java
+++ IndividualAssignment/src/A1_Pizzeria.java
@@ -32,41 +32,57 @@ public class A1_Pizzeria extends HttpServlet {
 		response.setContentType("text/html");
 		java.io.PrintWriter out = response.getWriter();
 		// start to print HTML page
-		out.println("<html><head><title>Welcome to Pizzeria</title>");
-		out.println("<style>h3{color:#0000ff;}#submit{width:150px;padding:0,10px}</style></head>");
-		out.println("<body><div align=\"center\"><h1>Welcome to Pizzeria</h1>");
-		out.println("<h3>Today's Special: Get 4$ off for any 3 or more medium pizzas of any type!</h3>");
+		out.println("<html><head><title>Pizzeria</title>");
+		out.println("<style>" 
+		+ "#background {width:500px;height:650px;background-color:#EEE;margin: 50px auto;}"
+		+ "#welcome {position:relative;top:50px;text-align:center;}"
+		+ "#todayspecial {position:relative;top:70px;color:#00F;text-align:center;}"
+		+ "#menu {position:relative;top:90px;text-align:center;}"
+		+ ".menu {margin:0 auto;line-height:250%;border:1px solid #000;}"
+		+ "#submit {position:relative;top:110px;text-align:center;}"
+		+ "#submit input {width:200px;height:200px;font-size:150%;}"
+		+ "</style></head>");
+		out.println("<body><div id=\"background\"><div id=\"welcome\"><h1>Welcome to Pizzeria</h1></div>");
+		out.println("<div id=\"todayspecial\"><h3><p>Today's Special:</p><p>Get 4$ off for any 3 or more medium pizzas of any type!</p></h3></div>");
 		// Create a HTML form with post method
+		out.println("<div id=\"menu\"><h3>MENU</h3>");
 		out.println("<form method=\"post\" action =\""
 				+ request.getContextPath() + "/A1_Pizzeria\" >");
-		out.println("<table><thead><tr><th>Type</th><th>Size</th><th>Price</th><th>Quantity</th></tr>");
+		out.println("<table class=\"menu\"><thead><tr><th>Type</th><th>Size</th><th>Price</th><th>Quantity</th></tr>");
 		// input box: number of large cheese pizza
 		out.println("<tbody><tr><td>Cheese Pizza</td><td>Large</td><td>$9.00</td>");
-		out.println("<td><input type=\"text\" name=\"numoflarche\"></td></tr>");
+		out.println("<td><input type=\"text\" name=\"numoflarche\" size=\"3\"></td></tr>");
 		// input box: number of medium cheese pizza
 		out.println("<tr><td></td><td>Medium</td><td>$7.50</td>");
-		out.println("<td><input type=\"text\" name=\"numofmedche\"></td></tr>");
+		out.println("<td><input type=\"text\" name=\"numofmedche\" size=\"3\"></td></tr>");
 		// input box: number of large pepperoni pizza
 		out.println("<tr><td>Pepperoni Pizza</td><td>Large</td><td>$10.50</td>");
-		out.println("<td><input type=\"text\" name=\"numoflarpep\"></td></tr>");
+		out.println("<td><input type=\"text\" name=\"numoflarpep\" size=\"3\"></td></tr>");
 		// input box: number of medium pepperoni pizza
 		out.println("<tr><td></td><td>Medium</td><td>$8.50</td>");
-		out.println("<td><input type=\"text\" name=\"numofmedpep\"></td></tr>");
+		out.println("<td><input type=\"text\" name=\"numofmedpep\" size=\"3\"></td></tr>");
 		// input box: number of medium buffalo wings
 		out.println("<tr><td>Buffalo Wings</td><td>Dozen</td><td>$6.00</td>");
-		out.println("<td><input type=\"text\" name=\"numofbuf\"></td></tr></tbody>");
+		out.println("<td><input type=\"text\" name=\"numofbuf\" size=\"3\"></td></tr></tbody></table></div>");
 		// Submit button
-		out.println("<tfoot><tr><td colspan=\"4\" align=\"center\"><input type=\"submit\" value=\"Submit your order\" id=\"submit\"></td></tr></tfoot>");
-		out.println("</table></form></div></body></html>");
+		out.println("<div id=\"submit\"><input type=\"submit\" value=\"Submit your order\"></div>");
+		out.println("</form></div></body></html>");
 
 	}
 
-	// method: apply discount or not
+	/**
+	 * method: apply discount or not
+	 * it is easy to use if the today's special is changed
+	 */
 	public static boolean discount(String name[], int number[]) {
 		String todayspecial = "Medium";
+		int specialnumber = 3, ordernumber = 0;
 		boolean discount = false;
 		for (int i = 0; i < name.length; i++) {
-			if (name[i].contains(todayspecial) && number[i] >= 3) {
+			if (name[i].contains(todayspecial)) {
+				ordernumber += number[i];
+			}
+			if (ordernumber >= specialnumber) {
 				discount = true;
 				break;
 			}
@@ -81,72 +97,74 @@ public class A1_Pizzeria extends HttpServlet {
 	protected void doPost(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		// need 9 variables to compute the price
-		int numoflarche = 0, numofmedche = 0, numoflarpep = 0, numofmedpep = 0, numofbuf = 0;
+		// need 10 variables to compute the price
+		int numoflarche = 0, numofmedche = 0, numoflarpep = 0, numofmedpep = 0, numofbuf = 0, ordernum=0;
 		double discount, totalprice = 0, tax, totalpricewtax;
-		// get number of items from user using arrays to simplify codes
-		String numbername[] = { "numoflarche", "numofmedche", "numoflarpep",
-				"numofmedpep", "numofbuf" };
-		int itemnumber[] = { numoflarche, numofmedche, numoflarpep,
-				numofmedpep, numofbuf };
-		for (int i = 0; i < numbername.length; i++) {
-			if (request.getParameter(numbername[i]).equals(""))
-				itemnumber[i] = 0;
-			else
-				itemnumber[i] = Integer.parseInt(request
-						.getParameter(numbername[i]));
-		}
 		// Create HTML page for results
 		response.setContentType("text/html");
 		java.io.PrintWriter out = response.getWriter();
 		out.println("<html><head><title>Thank you for your order</title></head>");
-		out.println("<body><div align=\"center\"><h2>Order Summary</h2>");
-		out.println("<table><thead><tr><th>Item</th><th>Price</th><th>Quantity</th><th>Subtotal</th></tr></thead>");
-		// show users' order and compute total price before tax(only show the
-		// items which the number > 0 )
-		out.println("<tbody>");
-		String itemname[] = { "Large cheese pizza", "Medium cheese pizza",
-				"Large pepperoni pizza", "Medium pepperoni pizza",
-				"Buffalo Wings" };
-		double itemprice[] = { 9, 7.5, 10.5, 8.5, 6 };
-		NumberFormat currencyFormatter = NumberFormat
-				.getCurrencyInstance(Locale.US);
-		for (int j = 0; j < itemname.length; j++) {
-			if (itemnumber[j] > 0) {
-				out.println("<tr><td>"
-						+ itemname[j]
-						+ "</td><td>"
-						+ currencyFormatter.format(itemprice[j])
-						+ "</td><td>"
-						+ itemnumber[j]
-						+ "</td><td>"
-						+ currencyFormatter
-								.format(itemnumber[j] * itemprice[j])
-						+ "</td></tr>");
-				totalprice += itemnumber[j] * itemprice[j];
+		out.println("<style>"
+				+ "#background{width:500px;height:650px;background-color:#EEE;margin: 50px auto;}"
+				+ "#ordertitle{position:relative;top:50px;text-align:center;}"
+				+ ".undertitle{position:relative;top:70px;text-align:center;}"
+				+ ".ordertable{margin:0 auto;line-height:250%;border:1px solid #000;border-collapse:collapse;}"
+				+ ".blue{color:#00F;}"
+				+ ".bordertop{border-top:1px solid #000;}"
+				+ "</style></head>");
+		out.println("<body><div id=\"background\"><div id=\"ordertitle\"><h2>Order Summary</h2></div>");
+		// get number of items from user
+		String numbername[] = { "numoflarche", "numofmedche", "numoflarpep","numofmedpep", "numofbuf" };
+		int itemnumber[] = { numoflarche, numofmedche, numoflarpep, numofmedpep, numofbuf };
+		for (int i=0;i<numbername.length;i++) {
+			if (request.getParameter(numbername[i]).equals("")||request.getParameter(numbername[i]).equals("0")){
+				itemnumber[i] = 0;
+				ordernum+=0;
+			} else {
+				itemnumber[i] = Integer.parseInt(request.getParameter(numbername[i]));
+				if (itemnumber[i]<0) {
+					ordernum=999;
+					break;
+				}
+				else ordernum+=1;
 			}
 		}
-		out.println("</tbody>");
+		// check order number
+		if (ordernum == 0) out.println("<div class=\"undertitle\"><h3 class=\"blue\">Sorry, you do not order anything.</h3><h3 class=\"blue\">Please check your order.</h3></div>");
+		else if (ordernum >= 999) out.println("<div class=\"undertitle\"><h3 class=\"blue\">Sorry, you must type in a positive integer.</h3><h3 class=\"blue\">Please check your order.</h3></div>");
+		else {
+		out.println("<div class=\"undertitle\"><table class=\"ordertable\"><thead><tr><th>Item</th><th>Price</th><th>Quantity</th><th>Subtotal</th></tr></thead>");
+		out.println("<tbody>");
+		//only show the items which the number > 0
+		String itemname[] = { "Large cheese pizza", "Medium cheese pizza","Large pepperoni pizza", "Medium pepperoni pizza",
+				"Buffalo Wings" };
+		double itemprice[] = { 9, 7.5, 10.5, 8.5, 6 };
+		NumberFormat currencyFormatter = NumberFormat.getCurrencyInstance(Locale.US);
+		for (int i = 0; i < numbername.length; i++) {
+			if (itemnumber[i]>0) {
+				out.println("<tr><td>" + itemname[i] + "</td><td>" + currencyFormatter.format(itemprice[i])
+						+ "</td><td align=\"center\">" + itemnumber[i] + "</td><td>" + currencyFormatter.format(itemnumber[i] * itemprice[i])
+						+ "</td></tr>");
+				totalprice += itemnumber[i] * itemprice[i];
+			}
+		}
 		// apply the discount only when it is qualified
-		out.println("<tfoot");
 		if (discount(itemname, itemnumber)) {
 			discount = 4;
-			out.println("<tr><td colspan =\"3\">Discount:Today's Special</td><td>"
+			out.println("<tr><td colspan =\"3\" class=\"blue\">Discount:Today's Special</td><td class=\"blue\">-"
 					+ currencyFormatter.format(discount) + "</td></tr>");
-		} else
-			discount = 0;
+		} else discount = 0;
+		out.println("</tbody>");
 		// output
 		totalprice = totalprice - discount;
 		tax = totalprice * 0.08;
 		totalpricewtax = totalprice + tax;
-		out.println("<tr><td colspan =\"3\">Total before tax</td><td>"
+		out.println("<tfoot><tr><td colspan =\"3\" class=\"bordertop\">Total before tax</td><td class=\"bordertop\">"
 				+ currencyFormatter.format(totalprice) + "</td></tr>");
-		out.println("<tr><td colspan = \"3\">Tax</td><td>"
-				+ currencyFormatter.format(tax) + "</td></tr>");
-		out.println("<tr><td colspan = \"3\">Order total</td><td>"
-				+ currencyFormatter.format(totalpricewtax) + "</td></tr>");
-		out.println("</tfoot></table></body></html>");
-
+		out.println("<tr><td colspan = \"3\">Tax</td><td>" + currencyFormatter.format(tax) + "</td></tr>");
+		out.println("<tr><td colspan = \"3\"><b>Order total</b></td><td><b>" 
+		+ currencyFormatter.format(totalpricewtax) + "</b></td></tr>");
+		out.println("</tfoot></table><h3>Thank you for your order!</h3></div></body></html>");
 	}
-
+}
 }
